660d57de5f3c16af1f0c1dd38957c111fbf52561
Anderson Dávila <andersonfidel.14@gmail.com>
2024-10-24 22:36:41 -0400

    auth api accounts and handling errors

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,14 +1,27 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { db } from "@/db/drizzel";
 import { accounts } from "@/db/schema";
 
 const app = new Hono()
-  .get("/", async (c) => {
-    const data = await db.select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts);
+  .get(
+    "/",
+    clerkMiddleware(), 
+    async (c) => {
+      const auth = getAuth(c);
+
+      if (!auth) {
+        throw new HTTPException(401, {
+          res: c.json({ error: "Unauthorized" }, 401),
+        });
+      }
+
+      const data = await db.select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts);
 
     return c.json({ data });
   });
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
+import { HTTPException } from "hono/http-exception"
 
 import accounts from "./accounts"
 
@@ -7,6 +8,15 @@ const app = new Hono().basePath("/api")
 
 const routes = app
   .route("/accounts", accounts)
+
+app.onError((err, c) => {
+  if(err instanceof HTTPException) {
+    return err.getResponse()
+  }
+
+  return c.json({ error: "Internal Error" }, 500)
+})
+
 
 export const GET = handle(app)
 export const POST = handle(app)

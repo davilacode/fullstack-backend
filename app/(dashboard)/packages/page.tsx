186f310c10b6f4841228061d08595bacb0a08fa1
Anderson Dávila <andersonfidel.14@gmail.com>
2024-10-25 12:23:54 -0400

    Add packages form and methods

--- app/(dashboard)/packages/page.tsx
+++ app/(dashboard)/packages/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+"use client"
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
+import { useNewPackages } from "@/functions/packages/hooks/usePackages";
+import { useGetPackages } from "@/functions/packages/api/useGetPackages";
+import { columns } from "./columns"
+
+
+const Packages = () => {
+  const { onOpen } = useNewPackages();
+  const packagesQuery = useGetPackages();
+  const data = packagesQuery.data ?? [];
+
+  if (packagesQuery.isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-      <h2>Paquetes</h2>
+      <div className="flex py-2 flex-col lg:flex-row gap-y-2 border-b-2 border-stone-600 justify-between mb-4">
+        <h2 className="font-bold text-2xl">Envíos</h2>
+        <Button onClick={() => onOpen()}>
+          <Plus className="size-4" /> 
+          envío
+        </Button>
+      </div>
+      <DataTable columns={columns} data={data} />
     </div>
-  )
+  );
 }
+ 
+export default Packages;
--- app/api/[[...route]]/packages.ts
+++ app/api/[[...route]]/packages.ts
@@ -1,11 +1,13 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 
 import { db } from "@/db/drizzel";
-import { packages } from "@/db/schema";
+import { packages, clients, insertPackagesSchema } from "@/db/schema";
+import { aliasedTable, eq } from "drizzle-orm";
 
 const app = new Hono()
   .get(
@@ -18,17 +20,97 @@ const app = new Hono()
         return c.json({ error: "Unauthorized" }, 401)
       }
 
+      const senders = aliasedTable(clients, "senders")
+      const recipients = aliasedTable(clients, "recipients")
+
       const data = await db.select({
-          id: packages.id,
-          content: packages.content,
-        })
-        .from(packages)
+        id: packages.id,
+        content: packages.content,
+        senderId: packages.senderId,
+        recipientId: packages.recipientId,
+        senderName: senders.name,
+        recipientName: recipients.name,
+        from: packages.from,
+        to: packages.to,
+        hight: packages.hight,
+        width: packages.width,
+        large: packages.large,
+        weight: packages.weight,
+        trackingId: packages.trackingId,
+        type: packages.type,
+        status: packages.status,
+        createdAt: packages.createdAt,
+        updatedAt: packages.updatedAt
+      })
+      .from(packages)
+      .innerJoin(senders, eq(packages.senderId, senders.id))
+      .innerJoin(recipients, eq(packages.senderId, recipients.id));
 
       return c.json({ data });
   })
+  .get(
+    "/:id", 
+    clerkMiddleware(), 
+    zValidator("param", z.object({
+      id: z.string().optional()
+    })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Bad request" }, 401)
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401)
+      }
+
+      const data = await db.select({
+        id: packages.id,
+        content: packages.content,
+        senderId: packages.senderId,
+        recipientId: packages.recipientId,
+        from: packages.from,
+        to: packages.to,
+        hight: packages.hight,
+        width: packages.width,
+        large: packages.large,
+        weight: packages.weight,
+        trackingId: packages.trackingId,
+        type: packages.type,
+        status: packages.status,
+        createdAt: packages.createdAt,
+        updatedAt: packages.updatedAt
+      })
+      .from(packages)
+      .where(
+        eq(packages.id, id)
+      );
+
+      if (!data.length) {
+        return c.json({ error: "Not found" }, 404)
+      }
+
+      return c.json({ data: data[0] });
+  })
   .post(
     "/", 
-    clerkMiddleware(), 
+    clerkMiddleware(),
+    zValidator("json", insertPackagesSchema.pick({
+      content: true,
+      senderId: true,
+      recipientId: true,
+      from: true,
+      to: true,
+      hight: true,
+      width: true,
+      large: true,
+      weight: true,
+      trackingId: true,
+      type: true,
+      status: true
+    })),
     async (c) => {
       const auth = getAuth(c);
       const values = await c.req.json();
@@ -37,14 +119,86 @@ const app = new Hono()
         return c.json({ error: "Unauthorized" }, 401)
       }
 
-      const [data] = await db.insert(packages).values({
+      const data = await db.insert(packages).values({
         id: createId(),
         userId: auth.userId,
         ...values
       }).returning()
 
+      return c.json({ data: data[0] });
+  })
+  .patch(
+    "/:id", 
+    clerkMiddleware(),
+    zValidator("param", z.object({
+      id: z.string().optional()
+    })),
+    zValidator("json", insertPackagesSchema.pick({
+      content: true,
+      senderId: true,
+      recipientId: true,
+      from: true,
+      to: true,
+      hight: true,
+      width: true,
+      large: true,
+      weight: true,
+      trackingId: true,
+      type: true,
+      status: true
+    })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const values = await c.req.json();
 
-      return c.json({ data });
+      if (!id) {
+        return c.json({ error: "Bad request" }, 401)
+      }
+
+      if (!auth) {
+        return c.json({ error: "Unauthorized" }, 401)
+      }
+
+      const data = await db.update(packages).set(values).where(
+        eq(packages.id, id)
+      ).returning()
+
+      if (!data.length) {
+        return c.json({ error: "Not found" }, 404)
+      }
+
+      return c.json({ data: data[0] });
+  })
+  .delete(
+    "/:id", 
+    clerkMiddleware(),
+    zValidator("param", z.object({
+      id: z.string().optional()
+    })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Bad request" }, 401)
+      }
+
+      if (!auth) {
+        return c.json({ error: "Unauthorized" }, 401)
+      }
+
+      const data = await db.delete(packages).where(
+        eq(packages.id, id)
+      ).returning({
+        id: packages.id,
+      })
+
+      if (!data.length) {
+        return c.json({ error: "Not found" }, 404)
+      }
+
+      return c.json({ data: data[0] });
   })
 
 export default app;
